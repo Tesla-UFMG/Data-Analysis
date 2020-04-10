eb66fed158810b99d394b4a7a2f6686593a84c98
Matheus Dias <diasmatheus05@gmail.com>
2020-04-10 15:35:14 -0300

    teste

--- app.py
+++ app.py
@@ -141,8 +141,7 @@ app.layout = html.Div(children=[
                                                     html.H1(
                                                         children=[
                                                             'Opções de Plotagem'
-                                                        ]
-                                                        
+                                                        ]   
                                                     )
                                                 ]
                                             ),
@@ -255,30 +254,59 @@ app.layout = html.Div(children=[
                             #         )
                             #     ]
                             # ),
-                            # #Terceiro Tab (Configuraçoes)
-                            # dcc.Tab(
-                            #     label='Configurações',
-                            #     value='tab-3',
-                            #     children=[
-                            #         dcc.Graph(
-                            #             figure={
-                            #                 'data': [
-                            #                     {'x': [1, 2, 3], 'y': [2, 4, 3],
-                            #                         'type': 'bar', 'name': 'SF'},
-                            #                     {'x': [1, 2, 3], 'y': [5, 4, 3],
-                            #                     'type': 'bar', 'name': u'Montréal'},
-                            #                 ]
-                            #             }
-                            #         )
-                            #     ]
-                            # )
+                            #Terceiro Tab (Configuraçoes)
+                            dcc.Tab(
+                                label='Configurações',
+                                value='tab-3',
+                                children=[
+                                    # Conteudo Terceiro Tab
+                                    html.Div(
+                                        className='config-page',
+                                        children=[
+                                            # Título
+                                            html.Div(
+                                                className="config-title",
+                                                children=[
+                                                    html.H1(
+                                                        children=[
+                                                            'Configurações Gerais'
+                                                        ]   
+                                                    )
+                                                ]
+                                            ),
+                                            html.Div(
+                                                className="divisão-content",
+                                                children=[
+                                                    # Opção para selecionar divisão por distancia ou tempo
+                                                    html.H4(
+                                                        children='Divisão de Voltas',
+                                                        className='form-label'
+                                                    ),
+                                                    # Checklist de Distancia ou Tempo
+                                                    dcc.Checklist(
+                                                        id='divisao-checklist',
+                                                        options=[
+                                                            {'label': 'Distância', 'value': 'Distancia'},
+                                                            {'label': 'Tempo', 'value': 'Tempo'}
+                                                        ],
+                                                        inputStyle = {'margin-right':'3px'},
+                                                        labelStyle =  {'margin-right':'8px'},
+                                                        value=[]
+                                                    ),
+                                                ]
+                                            ),
+                                        ]
+                                    ), 
+                                ]
+                            )
                         ]
                     )
                 ]
             )
         ]
     ),
-    # Layout botãp de Configurações avançadas
+
+    # Layout botão de Configurações avançadas
     dbc.Modal(
         contentClassName = 'modal-content',
         children = [
@@ -302,7 +330,7 @@ app.layout = html.Div(children=[
     )
 ])
 
-#Dicionário(HASH) com todas as unidades dos dados conhecidos
+# Dicionário(HASH) com todas as unidades dos dados conhecidos
 unidades_dados_hash = {
     'Intensidade_Frenagem': '%',
     'Timer': 's',
@@ -346,7 +374,7 @@ unidades_dados_hash = {
     'Hodometro_T': 'm'
 }
 
-#Dicionário(HASH) com todas as funções de conversão de unidades de cada dado 
+# Dicionário(HASH) com todas as funções de conversão de unidades de cada dado 
 tratamento_dados_hash = {
     'Intensidade_Frenagem': lambda x: x/10,
     'Timer': lambda x: x/1000,
@@ -359,7 +387,7 @@ tratamento_dados_hash = {
     'Volante': lambda x: (x-1030)/10
 }
 
-#Lista com todos os possíveis dados a serem analisados no software
+# Lista com todos os possíveis dados a serem analisados no software
 data_name = [
     'ECU_Mode',
     'Intensidade_Frenagem',
@@ -431,16 +459,20 @@ data_name = [
     'IRCan[3]'
 ]
 
-#Funçao de Media Movel
+# Funçao de Media Movel
 def smooth(y, box_pts):
+
     box = np.ones(box_pts)/box_pts
     y_smooth = np.convolve(y, box, mode='same')
+
     return y_smooth
 
-#Função que faz as conversões de unidade nos dados dos arquivos, aplicando cada função da tabela hash de conversão no seu respectivo dado
+# Função que faz as conversões de unidade nos dados dos arquivos, aplicando cada função da tabela hash de conversão no seu respectivo dado
 def trataDados(selected_x, selected_y):
+
     global data
     global converted_data
+    
     selected_y_copy = selected_y.copy()
 
     if not(selected_x in selected_y_copy):
@@ -450,48 +482,65 @@ def trataDados(selected_x, selected_y):
         if not(coluna in converted_data):
             if(coluna in tratamento_dados_hash):
                 data[coluna] = tratamento_dados_hash[coluna](data[coluna])
+
             converted_data.append(coluna)    
 
-#Funções de filtro passa-banda
+# Funções de filtro passa-banda
 def butter_bandpass(lowcut, highcut, fs, order=5):
+    
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
     b, a = signal.butter(order, [low, high], btype='band')
+
     return b, a
+
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = signal.lfilter(b, a, data)
+    
     return y
 
-#Função utilizada nos callbacks de abrir/fechar os bootstrap collapses dentro do modal de config avançadas
+# Função utilizada nos callbacks de abrir/fechar os bootstrap collapses dentro do modal de config avançadas
 def generate_toggle_callback():
+
     def toggle_collapse(n, is_open):
+
         if n:
             return not is_open
+        
         return is_open
+    
     return toggle_collapse
 
-#Função utilizada nos callbacks de habilitar/desabilitar inputs numéricos do filtro passa-banda
+# Função utilizada nos callbacks de habilitar/desabilitar inputs numéricos do filtro passa-banda
 def generate_input_passabanda_disable_callback():
+
     def disable_inputs_passabanda(checkbox):
+
         if( 'Passa-Banda' in checkbox):
             return [False,False]
         else:
             return [True,True]
+
     return disable_inputs_passabanda
 
-#Função utilizada nos callbacks de habilitar/desabilitar inputs numéricos do filtro Savitzky-golay
+# Função utilizada nos callbacks de habilitar/desabilitar inputs numéricos do filtro Savitzky-golay
 def generate_input_savitzky_disable_callback():
+
     def disable_inputs_savitzky(checkbox):
+
         if( 'Filtro savitzky-golay' in checkbox):
             return [False,False]
         else:
             return [True,True]
+
     return disable_inputs_savitzky
 
-#Função que cria o corpo HTML do modal. Cada Chamada dessa função retorna um Bootstrap collapse para o dado passado como parâmetro
+# Função que cria o corpo HTML do modal. Cada Chamada dessa função retorna um Bootstrap collapse para o dado passado como parâmetro
 def generate_element_modal_body(num, column_name):
+
     html_generated = [
         html.Div(
             id= num + '-modal-element',
@@ -584,16 +633,18 @@ def generate_element_modal_body(num, column_name):
             ]
         )
     ]
+
     return html_generated
 
-#Desativa as exceptions ligadas aos callbacks, permitindo a criação de callbacks envolvendo IDs que ainda não foram criados
+
+# Desativa as exceptions ligadas aos callbacks, permitindo a criação de callbacks envolvendo IDs que ainda não foram criados
 app.config['suppress_callback_exceptions'] = True
 
-
-
-#Laço para criar todos os callbacks possíveis, de forma a contornar a impossibilidade de criar callbacks dinâmicamente
+# Laço para criar todos os callbacks possíveis, de forma a contornar a impossibilidade de criar callbacks dinâmicamente
 for num in range(num_max_dados):
+    
     num = str(num)
+
     app.callback(
         Output( num + "-collapse" , "is_open"),
         [Input(num + "-collapse-button", "n_clicks")],
@@ -620,15 +671,17 @@ for num in range(num_max_dados):
     ])
 
 
-#Callback de abrir/fechar o modal de configurações avançadas
+# Callback de abrir/fechar o modal de configurações avançadas
 @app.callback(
     Output("modal-graph-config", "is_open"),
     [Input("modal-button", "n_clicks"), Input("close-modal", "n_clicks")],
     [State("modal-graph-config", "is_open")],
 )
 def toggle_modal(open_button, close_button, is_open):
+
     if open_button or close_button:
         return not is_open
+    
     return is_open
 
 # Callback para o Upload de arquivos, montagem do dataFrame e do html do modal
@@ -638,10 +691,14 @@ def toggle_modal(open_button, close_button, is_open):
     [State('upload-data', 'filename')]
 )
 def hide_index_and_read_file(list_of_contents, list_of_names):
+
     global data
     global num_dados
+    
     if list_of_contents is not None:
+        
         if ('legenda.txt' in list_of_names) and (len(list_of_names) >= 2):
+            
             files = dict(zip(list_of_names, list_of_contents))
             legenda = pd.read_csv(io.StringIO(base64.b64decode(files['legenda.txt'].split(',')[1]).decode('utf-8')))
             legenda = [name.split()[0][0].upper() + name.split()[0][1:] for name in legenda.columns.values]
@@ -649,9 +706,11 @@ def hide_index_and_read_file(list_of_contents, list_of_names):
             for nome_do_arquivo in files:
                 if(nome_do_arquivo != 'legenda.txt'):
                     data = pd.read_csv(io.StringIO(base64.b64decode(files[nome_do_arquivo].split(',')[1]).decode('utf-8')), delimiter='\t', names=legenda, index_col=False)
+            
             options = []
             modalbody_content = []
             num_dados = len(legenda)
+            
             for cont, column_name in enumerate(legenda):
                 options.append( {'label' : column_name, 'value' : column_name} )
                 all_data_name[column_name] = cont
@@ -659,71 +718,142 @@ def hide_index_and_read_file(list_of_contents, list_of_names):
                 output_for_apply_callback.extend([
                     Output(str(cont) + '-modal-element', 'style')
                 ])
+            
             for cont in range(num_dados,num_max_dados):
                 modalbody_content.extend(generate_element_modal_body(str(cont), 'extra'))
+            
             return [{'display': 'none'}, {'display':'inline'}, options, options, modalbody_content]
     else:
         raise PreventUpdate
             
 
-#Callback que habilita e desabilita o INPUT de média móvel
+# Callback que habilita e desabilita o INPUT de média móvel
 @app.callback(
     Output('media-movel-input','disabled'),
     [Input('filtros-checklist','value')]
 )
 def disable_media_movel_input(selected_filters):
+    
     if ('Média Móvel' in selected_filters) or ('Filtro Mediana' in selected_filters):
         return False
     else:
         return True
 
-state_for_apply_callback[0:0] = [State('dropdown-analise-geral-Y','value'),State('dropdown-analise-geral-X','value'), State('filtros-checklist','value'), State('media-movel-input','value')]
-output_for_apply_callback[0:0] = [Output('Graph-content','children'), Output('modal-button','style')]
+state_for_apply_callback[0:0] = [State('dropdown-analise-geral-Y','value'),
+                                 State('dropdown-analise-geral-X','value'), 
+                                 State('filtros-checklist','value'), 
+                                 State('media-movel-input','value')
+                                ]
+output_for_apply_callback[0:0] = [Output('Graph-content','children'), 
+                                  Output('modal-button','style')
+                                 ]
 
 
-#Callback do botão de plotagem de graficos
+# Callback do botão de plotagem de graficos
 @app.callback(
     output_for_apply_callback,
     [Input('plot-button','n_clicks_timestamp'), Input('apply-adv-changes-button','n_clicks_timestamp')],
     state_for_apply_callback
 )
 def plot_graph_analise_geral(button_plot, button_apply, selected_columns_Y, selected_X, filters, filters_subseq, *args):
+    
     global data_copy
     global show_modal_items
+    
     if button_plot != 0 or button_apply != 0:
+
         if int(button_plot) > int(button_apply):
+
             trataDados(selected_X, selected_columns_Y)
             data_copy = data.copy()
+
             if filters_subseq % 2 == 0:
                 filters_subseq = filters_subseq + 1
+
             if ('Filtro Mediana' in filters) and ('Média Móvel' in filters):
+
                 for column in selected_columns_Y:
                     data_copy[column] = smooth(signal.medfilt(data_copy[column], filters_subseq), filters_subseq)
             elif 'Média Móvel' in filters:
+
                 for column in selected_columns_Y:
                     data_copy[column] = smooth(data_copy[column], filters_subseq)
             elif 'Filtro Mediana' in filters:
+
                 for column in selected_columns_Y:
                     data_copy[column] = signal.medfilt(data_copy[column], np.array(filters_subseq))
         elif(int(button_plot) < int(button_apply)):
+
             for column in selected_columns_Y:
                 data_index = all_data_name[column] * 6
+                
                 if( 'Passa-Banda' in args[data_index] ):
-                    data_copy[column] = butter_bandpass_filter(data_copy[column], args[data_index+1], args[data_index+2], fs=60)
+                    data_copy[column] = butter_bandpass_filter(data_copy[column], 
+                                                               args[data_index+1],
+                                                               args[data_index+2],
+                                                               fs=60
+                                                              )
+
                 if( 'Filtro savitzky-golay' in args[data_index+3]):
+                    
                     window_length = args[data_index+4]
+
                     if window_length % 2 == 0:
                         window_length += 1
-                    data_copy[column] = signal.savgol_filter(data_copy[column], window_length=window_length, polyorder=args[data_index+5])
+
+                    data_copy[column] = signal.savgol_filter(data_copy[column],
+                                                             window_length=window_length,
+                                                             polyorder=args[data_index+5]
+                                                            )
 
         show_modal_items_copy = show_modal_items[:num_dados]
-        fig = make_subplots(rows=len(selected_columns_Y), cols=1, shared_xaxes=True, vertical_spacing=0.0)
+        fig = make_subplots(rows=len(selected_columns_Y),
+                            cols=1, 
+                            shared_xaxes=True, 
+                            vertical_spacing=0.0
+                           )
+
         for cont, column_name in enumerate(selected_columns_Y):
             if (column_name in unidades_dados_hash):
-                fig.add_trace(go.Scatter(y=data_copy[column_name], x=data_copy[selected_X], mode="lines", name=column_name, hovertemplate = "%{y} " + unidades_dados_hash[column_name]), row=cont+1, col=1)
+                fig.add_trace(go.Scatter(y=data_copy[column_name], 
+                                         x=data_copy[selected_X], 
+                                         mode="lines", 
+                                         name=column_name, 
+                                         hovertemplate = "%{y} " + unidades_dados_hash[column_name]
+                                        ), 
+                              row=cont+1, 
+                              col=1
+                             )
             else:
-                fig.add_trace(go.Scatter(y=data_copy[column_name], x=data_copy[selected_X], mode="lines", name=column_name, hovertemplate = "%{y}"), row=cont+1, col=1)               
+                fig.add_trace(go.Scatter(y=data_copy[column_name], 
+                                         x=data_copy[selected_X],
+                                         mode="lines", 
+                                         name=column_name, 
+                                         hovertemplate = "%{y}"
+                                        ), 
+                              row=cont+1, 
+                              col=1
+                             )
+
             show_modal_items_copy[all_data_name[column_name]] = {'display':'block'}
+            #print(selected_columns_Y)
+            #print(data_copy[column_name])
+
+        # tempo_voltas = [50, 100, 150]
+
+        # Acresenta traços de divisão de voltas
+        for cont, column_name in enumerate(selected_columns_Y):
+            for z in range(50, 300, 50):
+                fig.add_trace(go.Scatter(y=[min(data_copy[column_name]), max(data_copy[column_name])],   # linha reta do valor minimo ao maximo do dado 
+                                         x=[z, z],                                                       # array com os valores dos tempos das voltas 
+                                         mode="lines", 
+                                         line=go.scatter.Line(color="gray"), 
+                                         showlegend=False
+                                        ),
+                              row=cont+1,
+                              col=1
+                             )
+                        
         fig['layout'].update(height=120*len(selected_columns_Y)+100, margin={'t':50, 'b':50, 'l':100, 'r':100})
 
         retorno = [
@@ -735,12 +865,11 @@ def plot_graph_analise_geral(button_plot, button_apply, selected_columns_Y, sele
             {'display':'inline'}
         ]
         retorno.extend(show_modal_items_copy)
+        
         return retorno
     else:
         #TRATAR ERRO
         raise PreventUpdate
-
-
 
 
 
